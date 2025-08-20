2a63e2ce479306fda3ef4ee037521991f8a0d456
vantrilove4 <nvantri93@gmail.com>
2025-08-20 16:30:33 +0700

    fix sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://qrfree.io.vn",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://qrfree.io.vn/about",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    // thêm các trang khác nếu có
+  ];
+}
